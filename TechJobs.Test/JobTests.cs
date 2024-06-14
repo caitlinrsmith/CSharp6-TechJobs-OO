12653394e5b59165209bed2373da1bc6ff6c34f8
caitlinrsmith <catie12smith@gmail.com>
2024-06-13 21:36:27 -0500

    finished task 4

--- TechJobs.Test/JobTests.cs
+++ TechJobs.Test/JobTests.cs
@@ -1,17 +1,71 @@
-﻿
-namespace TechJobs.Tests
+﻿namespace TechJobs.Tests
 {
-	[TestClass]
-	public class JobTests
-	{
+    [TestClass]
+    public class JobTests
+    {
         //Testing Objects
-        //initalize your testing objects here
+        Job job1 = new Job();
+
+        Job job2 = new Job();
+
+        Job job3 = new Job(
+            "Product tester",
+            new Employer("ACME"),
+            new Location("Desert"),
+            new PositionType("Quality control"),
+            new CoreCompetency("Persistence")
+        );
+
+        Job job4 = new Job(
+            "Product tester",
+            new Employer("ACME"),
+            new Location("Desert"),
+            new PositionType("Quality control"),
+            new CoreCompetency("Persistence")
+        );
 
         [TestMethod]
-        public void TestMethod()
+        public void TestSettingJobId()
         {
-            //TODO: Task 4: remove this method before you create your first test method 
+            //verifies that the condition is false, meaning the values should not be equal
+            bool areEqual = job1.Id == job2.Id;
+
+            Assert.IsFalse(areEqual, $"The values {job1} and {job2} should not be equal.");
+
+            Assert.IsTrue(
+                job2.Id - job1.Id == 1,
+                $"The values {job1.Id} and {job2.Id} should have IDs that differ by 1."
+            );
+        }
+
+        [TestMethod]
+        public void TestJobConstructorSetsAllFields()
+        {
+            Assert.AreEqual(
+                "Product tester",
+                job3.Name,
+                $"{job3.Name} should be equal to 'product tester'."
+            );
+            Assert.AreEqual(
+                "ACME",
+                job3.EmployerName.Value,
+                $"{job3.EmployerName} should be equal to 'ACME'."
+            );
+            Assert.AreEqual(
+                "Desert",
+                job3.EmployerLocation.Value,
+                $"{job3.EmployerLocation} should be equal to 'Desert'."
+            );
+            Assert.AreEqual(
+                "Quality control",
+                job3.JobType.Value,
+                $"{job3.JobType} should be equal to 'Quality control.'"
+            );
+            Assert.AreEqual(
+                "Persistence",
+                job3.JobCoreCompetency.Value,
+                $"{job3.JobCoreCompetency} should be equal to 'Persistence'."
+            );
         }
     }
 }
-
